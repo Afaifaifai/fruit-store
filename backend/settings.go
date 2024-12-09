731ce02cc902d13a3e9053fead5bcb4ade180d05
xleshing <xleshing21@gmail.com>
2024-12-09 16:39:03 +0000

    Init

--- backend/settings.go
+++ backend/settings.go
@@ -1,17 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-const USERNAME string = "root"
-const PASSWORD string = "0000"
-const IP string = "127.0.0.1"
-const SQL_PORT string = "3306"
 const LISTEN_PORT string = "0.0.0.0:8080"
 
-var dsn string = fmt.Sprintf("%s:%s@tcp(%s:%s)/", USERNAME, PASSWORD, IP, SQL_PORT) // username:password@tcp(ip:port)/mysql
+var USERNAME string = os.Getenv("DB_USER")       // 從環境變數中讀取
+var PASSWORD string = os.Getenv("DB_PASSWORD")   // 從環境變數中讀取
+var IP string = os.Getenv("DB_HOST")             // 從環境變數中讀取
+var SQL_PORT string = os.Getenv("DB_PORT")       // 從環境變數中讀取
+
+var dsn string = fmt.Sprintf("%s:%s@tcp(%s:%s)/", USERNAME, PASSWORD, IP, SQL_PORT)
 
 const DATABASE_NAME string = "fruit_store"
-
 const RESET_DATABASE bool = true
 
 var Tables []Table = []Table{fruits, members, inactive, suppliers, transactions}
+
